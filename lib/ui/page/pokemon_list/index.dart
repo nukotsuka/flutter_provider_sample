e8444ab4b0d573b6a49f19222a8bfb372ad8b7fa
nukotsuka <nukotsuka@gmail.com>
2021-08-31 22:34:07 +0900

    feat: Create tmp PokemonListPage

---
+++ lib/ui/page/pokemon_list/index.dart
@@ -0,0 +1,1 @@
+export 'pokemon_list_page.dart';
---
+++ lib/ui/page/pokemon_list/pokemon_list_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PokemonListPage extends StatelessWidget {
+  const PokemonListPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar());
+  }
+}
