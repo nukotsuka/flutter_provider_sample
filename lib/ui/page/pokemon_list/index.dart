e74c175607e4a64b37c31355de25df771b30f486
nukotsuka <nukotsuka@gmail.com>
2021-09-01 19:55:24 +0900

    feat: Create PokemonListNotifier

--- lib/ui/page/pokemon_list/index.dart
+++ lib/ui/page/pokemon_list/index.dart
@@ -1,2 +1,3 @@
+export 'pokemon_list_notifier.dart';
 export 'pokemon_list_page.dart';
 export 'pokemon_list_state.dart';
---
+++ lib/ui/page/pokemon_list/pokemon_list_notifier.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_provider_sample/domain/index.dart';
+import 'package:state_notifier/state_notifier.dart';
+
+import 'pokemon_list_state.dart';
+
+class PokemonListNotifier extends StateNotifier<PokemonListState>
+    with LocatorMixin {
+  // MARK: - Variables
+
+  late PokemonService _pokemonService;
+
+  // MARK: - Constructor
+
+  PokemonListNotifier() : super(const PokemonListState());
+
+  // MARK: - Life Cycle
+
+  @override
+  Future<void> initState() async {
+    super.initState();
+
+    _pokemonService = read();
+
+    await _loadPokemons();
+  }
+
+  // MARK:- Service Methods
+
+  Future<void> _loadPokemons() async {
+    state = state.copyWith(isLoading: true);
+    try {
+      final pokemons = await _pokemonService.getAll();
+      state = state.copyWith(pokemons: pokemons);
+    } finally {
+      state = state.copyWith(isLoading: false);
+    }
+  }
+}
