f58751a27cb1622a9f942f585e0d4e1d78fd7cc5
nukotsuka <nukotsuka@gmail.com>
2021-09-01 20:42:27 +0900

    feat: Add PokemonTypeEx.colorCode

--- lib/domain/entity/pokemon_type.dart
+++ lib/domain/entity/pokemon_type.dart
@@ -66,4 +66,45 @@ extension PokemonTypeEx on PokemonType {
         return 'フェアリー';
     }
   }
+
+  int get colorCode {
+    switch (this) {
+      case PokemonType.Normal:
+        return 0xFF8E979F;
+      case PokemonType.Fighting:
+        return 0xFFCA4069;
+      case PokemonType.Flying:
+        return 0xFF8FA8DD;
+      case PokemonType.Poison:
+        return 0xFFA064C3;
+      case PokemonType.Ground:
+        return 0xFFD27546;
+      case PokemonType.Rock:
+        return 0xFFC3B589;
+      case PokemonType.Bug:
+        return 0xFF8CBC2B;
+      case PokemonType.Ghost:
+        return 0xFF5165A5;
+      case PokemonType.Steel:
+        return 0xFF508397;
+      case PokemonType.Fire:
+        return 0xFFFB9B54;
+      case PokemonType.Water:
+        return 0xFF5090D4;
+      case PokemonType.Grass:
+        return 0xFF5DB351;
+      case PokemonType.Electric:
+        return 0xFFEECD3A;
+      case PokemonType.Psychic:
+        return 0xFFF26C72;
+      case PokemonType.Ice:
+        return 0xFF6EC7B9;
+      case PokemonType.Dragon:
+        return 0xFF0B65BB;
+      case PokemonType.Dark:
+        return 0xFF5A5365;
+      case PokemonType.Fairy:
+        return 0xFFE889DF;
+    }
+  }
 }
