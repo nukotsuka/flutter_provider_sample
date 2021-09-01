62d8ec1487e14fe42d3d4bc3af05dddfcd47c470
nukotsuka <nukotsuka@gmail.com>
2021-09-01 19:37:52 +0900

    feat: Add Pokemon.imageURL

--- lib/domain/entity/pokemon/pokemon.dart
+++ lib/domain/entity/pokemon/pokemon.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_provider_sample/app_config.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 import '../pokemon_name/pokemon_name.dart';
@@ -16,4 +17,9 @@ class Pokemon with _$Pokemon {
 
   factory Pokemon.fromJson(Map<String, dynamic> json) =>
       _$PokemonFromJson(json);
+}
+
+extension PokemonEx on Pokemon {
+  String get imageURL =>
+      '${AppConfig.pokemonApiEndpoint}/images/${id.toString().padLeft(3, "0")}.png';
 }
