4b1ce60b79e3f6c4081c7472356911e74cd379c6
nukotsuka <nukotsuka@gmail.com>
2021-09-02 01:20:32 +0900

    test: Create mocks

---
+++ test/mock/index.dart
@@ -0,0 +1,1 @@
+export 'mock.mocks.dart';
---
+++ test/mock/mock.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_provider_sample/domain/index.dart';
+import 'package:mockito/annotations.dart';
+
+@GenerateMocks([
+  // Entity
+  Pokemon,
+
+  // Repository
+  PokemonRepository,
+])
+void main() {}
